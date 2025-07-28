9797d5ee9f4a15e89b476ba6c0bd29da7dc00dc0
Kunal Chheda <kunalchheda13@gmail.com>
2025-07-28 18:58:12 +0000

    this works here but in vercel there are erros

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-
+/** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // You can add other Next.js configurations here if needed.
 };
 
 const pwaConfig = {
@@ -14,6 +12,4 @@ const pwaConfig = {
   disable: process.env.NODE_ENV === 'development',
 };
 
-// The 'withPWA' function needs to be called with the PWA config,
-// and it returns a new function that you then call with the Next.js config.
 export default withPWA(pwaConfig)(nextConfig);
